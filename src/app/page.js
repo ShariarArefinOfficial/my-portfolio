dfb4e44014f6247ea3b55172ffa8e13a0bad9af0
ShariarArefinOfficial <shariararefin0@gmail.com>
2024-07-03 15:36:05 +0600

    Working on Hero Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
-
+import Hero from '@/components/Hero/Hero';
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      content
+    <main>
+      <section className='h-full' >
+        <Hero></Hero>
+      </section>
 
     </main>
   );
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {FiDownload} from 'react-icons/fi'
+
+
+const Hero = () => {
+    return (
+        <div className='container mx-auto'>
+          <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
+            <div>Text</div>
+            <div>Photo</div>
+          </div>
+
+        </div>
+    );
+};
+
+export default Hero;
