382414a4c2c7ee5e43ba845d50da327c7c960841
ShariarArefinOfficial <shariararefin0@gmail.com>
2024-07-05 02:02:14 +0600

    Pdf File integrate

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {FiDownload} from 'react-icons/fi'
 import { Button } from '../ui/button';
+import Link from 'next/link'; 
 
 //======Shared Component
 import Socials from '../Shared/Socials/Socials';
@@ -21,10 +22,12 @@ const Hero = () => {
                 I am a passionate Front-End Developer with a strong focus on creating visually appealing, user-friendly, and responsive web applications. With expertise in HTML, CSS, and JavaScript, alongside framework like React js, I bring innovative designs to life with seamless functionality. 
                 </p>
                 <div className='flex flex-col xl:flex-row items-center gap-8'>
-                  <Button size="lg" className=" uppercase flex items-center gap-2">
+                  <Link href={'/resume.pdf'}  download={'Resume'}>
+                  <Button size="lg" className=" uppercase flex items-center gap-2" >
                     <span className='mr-2'>Download Resume</span>
                     <FiDownload className='text-xl'></FiDownload>
                   </Button>
+                  </Link>
                   <div className='mb-8 xl:mb-0'>
                     <Socials
                     containerStyle={'flex gap-4'}
